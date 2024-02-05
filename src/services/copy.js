895ed2f709f24d87244e831b1718e160a8a23bd2
a-reznikov <aliaksandr.reznikau@pargoo.com>
2024-02-05 22:25:03 +0300

    feat: refactor method copy file

--- src/services/copy.js
+++ src/services/copy.js
@@ -1,9 +1,12 @@
-import { copyFile } from 'node:fs/promises';
 import { join } from 'path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
 
 export const copyFileToDestination = async (fileName, source, destination) => {
     try {
-        await copyFile(source, join(destination, fileName));
+        const streamRead = createReadStream(source, 'utf-8');
+        const streamWrite = createWriteStream(join(destination, fileName), 'utf-8');
+        await pipeline(streamRead, streamWrite);
         console.log('=== File was successfully copied! ===');
     } catch (error) {
         throw new error;
